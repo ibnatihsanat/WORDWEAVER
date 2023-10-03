e9b19543e22d9e4e96d56d090174b722155be0bc
khadijah <karmayauabubakar@gmail.com>
2023-10-03 21:56:15 +0100

    finished media query

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,15 +1,26 @@
-const menu = document.querySelector('.menu');
-        const MenuItem = document.querySelector('.menuitems');
-        const closeIcon = document.querySelector('.close-icon');
+// const menu = document.querySelector('.menu');
+//         const MenuItem = document.querySelector('.menuitems');
+//         const closeIcon = document.querySelector('.close-icon');
 
-        menu.addEventListener('click', (e) => {
-            MenuItem.style.display = 'block';
-            closeIcon.style.display = 'block';
-            menu.style.display = 'none';
-        });
+//         menu.addEventListener('click', (e) => {
+//             MenuItem.style.display = 'block';
+//             closeIcon.style.display = 'block';
+//             menu.style.display = 'none';
+//         });
 
-        closeIcon.addEventListener('click', (e) => {
-            MenuItem.style.display = 'none';
-            closeIcon.style.display = 'none';
-            menu.style.display = 'block';
-        });
+//         closeIcon.addEventListener('click', (e) => {
+//             MenuItem.style.display = 'none';
+//             closeIcon.style.display = 'none';
+//             menu.style.display = 'block';
+//         });
+
+function toggleMenu() {
+    const nav = document.querySelector('.header-nav');
+    nav.classList.toggle('show');
+}
+
+const closeIcon = document.createElement('div');
+closeIcon.className = 'close-icon';
+closeIcon.innerHTML = 'X';
+closeIcon.addEventListener('click', toggleMenu);
+document.querySelector('.header-nav').appendChild(closeIcon);
